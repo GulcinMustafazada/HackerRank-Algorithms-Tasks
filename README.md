0db250c918022e9b25d0b79378ae8a93eeb4f74f
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-04-18 22:27:34 +0400

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+## HackerRank-Algorithms-Tasks
+
+[My HackerRank Profile](https://www.hackerrank.com/gulcinmustafaza2)
---
+++ app.js
@@ -0,0 +1,7 @@
+let jsname = prompt("What is the official name of javascript?", "");
+if (jsname == 'Ecmascript') {
+    alert(true)
+} else {
+    alert("ecmascript oldugunu bilmiyormusun")
+}
+//javascriptinfo
